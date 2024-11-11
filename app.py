ffc580dd85380b53da090be96000216f507a80e0
talosak <talosak@hotmail.com>
2024-11-11 18:15:40 +0100

    added basic template and index.html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import flask
 import flask_session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world"
+    return flask.render_template("index.html")
