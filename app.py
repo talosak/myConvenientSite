df7746ed74c6a3defe29653e884a238d74da2bec
talosak <talosak@hotmail.com>
2024-11-11 19:10:37 +0100

    prepared to figure out how to add SQL

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import flask_session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = flask.Flask(__name__)
+app.config["SESSION_TYPE"] = "filesystem"
+flask_session.Session(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return flask.render_template("index.html")
+    if flask.request.method == "POST":
+        return flask.render_template("index.html")
+    else:
+        return flask.render_template("index.html")
+       
