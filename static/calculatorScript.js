16dee8388e0fc44c006f8f6d5e9029effdcdda6d
talosak <talosak@hotmail.com>
2024-11-22 17:55:03 +0100

    finished calculator

--- static/calculatorScript.js
+++ static/calculatorScript.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    var calculatorButtons = document.querySelectorAll(".btn-calculator");
+    var result = document.querySelector("#calculator-result")
+    calculatorButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            if (event.target.id === "calc-button-c") {
+                result.value = "";
+            } else if (event.target.id === "calc-button-=") {
+                result.value = math.eval(result.value)
+            } else {
+                result.value += event.target.value
+            };
+        });
+    });
  });
